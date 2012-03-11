377e5e71b606fcfbf063f41db1409a4812b021b2
zelon <zelonion@gmail.com>
2012-03-11 22:17:19 +0900

    Added popup mode and added default.html page

--- Chrome/common.js
+++ Chrome/common.js
@@ -1,5 +1,6 @@
 
 var baseUrlName = "baseurl";
+var popupModeName = "popupmode";
 
 String.prototype.startsWith = function (str) {
         return !this.indexOf(str);
@@ -14,19 +15,49 @@ function $(id)
 	return document.getElementById(id);
 }
 
+function setBaseUrl(newUrl)
+{
+	localStorage["baseurl"] = newUrl;
+}
+
 function getBaseUrl()
 {
 	var baseUrl = localStorage[baseUrlName];
 	
 	if ( baseUrl == null )
 	{
-		baseUrl = "http://small.dic.daum.net/search.do?q=";
+		baseUrl = "http://dic.daum.net/search.do?q=";
 	}
 	
 	return baseUrl;
 }
 
+function setPopupMode(bOn)
+{
+	if ( bOn == true )
+	{
+		localStorage[popupModeName] = "true";
+	}
+	else
+	{
+		localStorage[popupModeName] = "false";
+	}
+}
+
+function isPopupMode()
+{
+	var popupMode = localStorage[popupModeName];
+	
+	if ( popupMode == null )
+	{
+		popupMode = "true";
+	}
+	
+	return (popupMode == "true");
+}
+
 function resetDefaultValues()
 {
 	localStorage.removeItem(baseUrlName);
+	localStorage.removeItem(popupModeName);
 }
